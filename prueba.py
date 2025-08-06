2079f594843acb2ff9c3646ca822ba78ef946e47
LeonelNieto <leonel.nieto.lara@gmail.com>
2025-08-06 12:20:26 -0600

    Refactor even_or_odd function

--- prueba.py
+++ prueba.py
@@ -2,16 +2,10 @@ def say_hello( name: str ) -> None:
     print( f"Hello, {name}!" )
 
 def even_or_odd( number: int ) -> None:
-    if number == 0:
-        print( "Even")
-    elif number == 1:
-        print( "Odd" )
-    elif number == 2:
-        print( "Even" )
-    elif number == 3:
-        print( "Odd" )
-    elif number == 4:
-        print( "Even" )
+    if number % 2 == 0:
+        print( f"{number} is even" )
+    else:
+        print( f"{number} is odd" )
 
 if __name__ == "__main__":
     say_hello( "Leonel" )
